7d525cc0fdc9de3ba0b0fe7afed65f4ae256064a
Sébastien Herbreteau <seb.eric.herbreteau@gmail.com>
2022-12-02 11:27:47 +0100

    gitignore

--- template-parts/dbco.php
+++ template-parts/dbco.php
@@ -1,8 +1,8 @@
 <?php
-$servname = 'localhost';
-$dbname = 'pokemon';
-$dbuser = 'root';
-$pass = '';
+$servname = 'sebastnfccg.mysql.db';
+$dbname = 'sebastnfccg';
+$dbuser = 'sebastnfccg';
+$pass = 'Cejuaner281215040412';
 $path = "mysql:host=$servname;dbname=$dbname;charset=utf8";
 $bdd = new PDO($path, $dbuser, $pass);
 $result = htmlspecialchars(addslashes($_GET['search'] ?? 'default'));
